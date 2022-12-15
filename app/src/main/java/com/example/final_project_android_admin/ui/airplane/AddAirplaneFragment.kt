43168eac9a0a8e226b3e69bb93f86d1b55a9eb53
Aiffah Kiysa Waafi <aiffahwaafi6@gmail.com>
2022-12-15 20:12:43 +0700

    add layout company

--- app/src/main/java/com/example/final_project_android_admin/ui/airplane/AddAirplaneFragment.kt
+++ app/src/main/java/com/example/final_project_android_admin/ui/airplane/AddAirplaneFragment.kt
@@ -1,4 +1,4 @@
-package com.example.final_project_android_admin.ui
+package com.example.final_project_android_admin.ui.airplane
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -14,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [AirportFragment.newInstance] factory method to
+ * Use the [AddAirplaneFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class AirportFragment : Fragment() {
+class AddAirplaneFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -35,7 +35,7 @@ class AirportFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_airport, container, false)
+        return inflater.inflate(R.layout.fragment_add_airplane, container, false)
     }
 
     companion object {
@@ -45,12 +45,12 @@ class AirportFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment AirportFragment.
+         * @return A new instance of fragment AddAirplaneFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            AirportFragment().apply {
+            AddAirplaneFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
--- app/src/main/java/com/example/final_project_android_admin/ui/airplane/AirplaneFragment.kt
+++ app/src/main/java/com/example/final_project_android_admin/ui/airplane/AirplaneFragment.kt
@@ -1,4 +1,4 @@
-package com.example.final_project_android_admin.ui.flight
+package com.example.final_project_android_admin.ui.airplane
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -14,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [AddFlightFragment.newInstance] factory method to
+ * Use the [AirplaneFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class AddFlightFragment : Fragment() {
+class AirplaneFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -35,7 +35,7 @@ class AddFlightFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_flight, container, false)
+        return inflater.inflate(R.layout.fragment_airplane, container, false)
     }
 
     companion object {
@@ -45,12 +45,12 @@ class AddFlightFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment AddFlightFragment.
+         * @return A new instance of fragment AirplaneFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            AddFlightFragment().apply {
+            AirplaneFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
